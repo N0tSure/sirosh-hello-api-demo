f27e08f14d707daf9a81ed1569ec558b55ab80e2
Artem Sirosh <asir2089@gmail.com>
2024-12-26 20:00:34 +0100

    SPSEC-01 Inital commit

---
+++ src/main/java/com/sirosh/spsec/key/ApiKeyAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.sirosh.spsec.key;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKeyAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiKeyAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sirosh/spsec/key/ApiKeyAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirosh.spsec.key;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKeyAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
